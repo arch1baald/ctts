858c44fd632419cad37339d78e20c9574660c469
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2025-05-11 23:58:19 -0700

    chore: UTTSClient and openai refactoring

--- src/utts/__init__.py
+++ src/utts/__init__.py
@@ -1,18 +1,10 @@
 """Universal Interface to Test and Compare Text-to-Speech models (UTTS)."""
 
-from utts import cartesia, config, elevenlabs, hume, kokoro, openai, orpheus, replicate, utils, zyphra
+from .client import UTTSClient
+from .utils import batch_generate, random_choice_enum
 
-# Expose key modules directly at the package level
-# This allows imports like `from utts import openai, elevenlabs`
 __all__ = [
-    "cartesia",
-    "config",
-    "elevenlabs",
-    "hume",
-    "kokoro",
-    "openai",
-    "orpheus",
-    "replicate",
-    "utils",
-    "zyphra",
+    "UTTSClient",
+    "batch_generate",
+    "random_choice_enum",
 ]
