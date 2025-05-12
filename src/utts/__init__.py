349bc2c562775d3e6facdf9e0fb602cfa7cd1282
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2025-05-12 00:54:17 -0700

    chore(cartesia): refactoring

--- src/utts/__init__.py
+++ src/utts/__init__.py
@@ -1,11 +1,11 @@
 """Universal Interface to Test and Compare Text-to-Speech models (UTTS)."""
 
-from . import elevenlabs, kokoro, orpheus, replicate, utils, zyphra  # , cartesia
+from . import cartesia, elevenlabs, kokoro, orpheus, replicate, utils, zyphra
 from .client import UTTSClient
 
 __all__ = [
     "UTTSClient",
-    # "cartesia",
+    "cartesia",
     "elevenlabs",
     "kokoro",
     "orpheus",
